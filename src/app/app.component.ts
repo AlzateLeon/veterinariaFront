2d1a46192fbfc5109660d3e06042d9a65e85a067
Juan Sebastian Alzate Leon <ing.jsalzateleon@gmail.com>
2023-09-20 18:41:00 -0600

    primer commit del proyecto front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-veterinaria';
+
+  constructor(private router: Router) { }
+
+  navegarAInicioUser() {
+    this.router.navigate(['/app-inicio-usuario']); // 'otro' es la ruta que definiste en el enrutamiento
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioUsuarioComponent } from './usuario/inicio-usuario/inicio-usuario.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioUsuarioComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
